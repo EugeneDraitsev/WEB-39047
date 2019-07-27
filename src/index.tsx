9c0af0c8e07562bf1b4b7439c731bb30c2b6d783
edraitsev <ddrrai@gmail.com>
2019-07-27 13:58:04 +0200

    Init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import styled from 'styled-components/macro'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BaseLayout = styled.div``
+
+const Wrapper = styled(BaseLayout)<{ param1?: string, param2?: string }>``
+
+ReactDOM.render((
+  <Wrapper>
+    <App />
+  </Wrapper>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
